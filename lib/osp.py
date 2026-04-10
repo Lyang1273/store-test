3fd04245295cc7b73cb5d1872e0d7b62308e076d
Lyang1273 <2609350581@qq.com>
2026-04-10 13:16:54 +0800

    读取APPInfo.json

--- lib/osp.py
+++ lib/osp.py
@@ -1,6 +1,7 @@
 from loguru import logger
 import zipfile
 import traceback
+import json
 
 
 def unzip(path, outputPath):
@@ -24,8 +25,32 @@ def unzip(path, outputPath):
     except zipfile.BadZipFile:
         logger.error(f"解压失败：压缩文件损坏或不是有效的ZIP文件 - {path}\n{traceback.format_exc()}")
         return False
-    except Exception as e:
+    except Exception:
         logger.error(f"解压失败：未知错误\n{traceback.format_exc()}")
         return False
+
+def APPInfo(path):
+    """
+    读取 APPInfo.json 中的内容
+    :param path: APPInfo.json 文件目录
+    :return: 读取成功时返回 data，失败时返回 None
+    """
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        print("✓ 文件读取成功")
+        return data
+
+    except FileNotFoundError:
+        print(f"找不到位于{path}的JSON文件")
+    except json.JSONDecodeError:
+        print(f"JSON解析错误")
+        return None
+    except Exception as e:
+        print(f"读取文件时出错，因为 {e}")
+        return None
+
+
 
 # unzip("../core1.py", "../cache")
